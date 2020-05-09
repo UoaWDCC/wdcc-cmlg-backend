48f157c9e04ffbb04b3ab07e6065b2c1bb377939
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-05-09 15:02:20 +1200

    [CMLG-003] creating feature test for searching keyword
    Issue:
    
    We need tests to automatically test the functionality of the endpoint /translations/{word}
    
    Solution:
    
    Creating a setup function that will automatically generate the required database used for testing.
    Calling the endpoint and search for different words, checking that the JSON response contains correct value.
    
    Four tests remained to be implemented.
    
    Risk area: /
    
    Reviewed by:
    Eileen, Annie

--- TranslationBackend/tests/Feature/WordTest.php
+++ TranslationBackend/tests/Feature/WordTest.php
@@ -2,41 +2,129 @@
 
 namespace Tests\Feature;
 
+use App\Language;
+use App\Translation;
+use App\Word;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class WordTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testChinese()
     {
-        $response = $this->get('/');
+        $response = $this->get('/translations/病毒');
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                "ZH CN" => [
+                    [
+                        "id" => "1",
+                        "name" => "病毒",
+                        "language_name" => "ZH CN",
+                        "translation_id" => "1"
+                    ]
+                ]
+            ]);
     }
 
-    
-    public function testSetUp()
+    public function testAccent()
     {
-        // generate two languages
+        $response = $this->get('/translations/bìngdú');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                "pinyin" => [
+                    [
+                        "id" => "2",
+                        "name" => "bìngdú",
+                        "language_name" => "pinyin",
+                        "translation_id" => "1"
+                    ]
+                ]
+            ]);
+    }
+
+    // @todo testing the /translation endpoint
+
+    // @todo test that two words with same languages are grouped together
+
+    // @todo test that two words with same translation id but different languages are not grouped together
+
+    // @todo test that when users search for non-exist words, empty result is returned
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        // generate three languages
         factory(Language::class)->create([
-            'id' => '1'
+            'id' => '1',
+            'name' => 'EN English'
         ]);
         factory(Language::class)->create([
-            'id' => '2'
+            'id' => '2',
+            'name' => 'ZH CN'
+        ]);
+        factory(Language::class)->create([
+            'id' => '3',
+            'name' => 'pinyin'
         ]);
 
-        // generate 5 words belonging to two different languages
-        factory(Word::class, 3)->create([
-            'language_id' => '1'
+        // generate three translations
+        factory(Translation::class)->create([
+            'id' => '1',
+            'name' => 'Virus'
         ]);
-        factory(Word::class, 2)->create([
-        'language_id' => '2'
+
+        factory(Translation::class)->create([
+            'id' => '2',
+            'name' => 'novel coronavirus'
         ]);
+
+        factory(Translation::class)->create([
+            'id' => '3',
+            'name' => 'Covid-19'
+        ]);
+
+        // generate 5 words belonging
+        factory(Word::class)->create([
+            'name' => '病毒',
+            'language_id' => '2',
+            'translation_id' => '1'
+        ]);
+
+        factory(Word::class)->create([
+            'name' => 'bìngdú',
+            'language_id' => '3',
+            'translation_id' => '1'
+        ]);
+
+        factory(Word::class)->create([
+            'name' => 'novel coronavirus',
+            'language_id' => '1',
+            'translation_id' => '2'
+        ]);
+
+        factory(Word::class)->create([
+            'name' => 'xīnxíng guānzhuàng bìngdú (xīnguān bìngdú)',
+            'language_id' => '3',
+            'translation_id' => '2'
+        ]);
+
+        factory(Word::class)->create([
+            'name' => 'fèiyán',
+            'language_id' => '3',
+            'translation_id' => '3'
+        ]);
+
     }
 }
